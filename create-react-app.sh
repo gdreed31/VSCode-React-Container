f16948e5400767940da7bf239badd4833f1ccd90
Gregg Reed <Gregg.Reed@e-gineering.com>
2021-08-23 09:40:36 -0400

    Added npm installs for various libraries

--- create-react-app.sh
+++ create-react-app.sh
@@ -1,4 +1,4 @@
-echo What is your application name?
+echo What is your application name?  all-lowercase-no-spaces
 read varname
 echo Running npx create-react-app $varname --template typescript
 npx create-react-app $varname --template typescript
